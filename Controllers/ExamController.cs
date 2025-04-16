aff9eda0f293deaa312bd8a1f354fdc6bb8bfecf
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-04-16 21:25:26 +0200

    fahmy - init

---
+++ Controllers/ExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+
+    }
+}
