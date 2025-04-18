81c73497c9e646da12dd8133ef74c91bd3ab17c6
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-04-18 23:27:40 +0200

    Exam & ExamQuestions Controllers

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Quiz.Models;
+using Quiz.Repository;
 
 namespace Quiz
 {
@@ -21,6 +22,10 @@ namespace Quiz
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Register the repository
+            builder.Services.AddScoped<ExamRepository>();
+            builder.Services.AddScoped<ExamQuestionsRepository>();
 
             var app = builder.Build();
 
